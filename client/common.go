b13d688a752a11cb55e61805a20623ad603cfa8a
Peter Grzybowski <peter@northern.tech>
2023-03-16 11:56:14 +0100

    feat: direct upload.
    Changelog: Title
    Ticket: MEN-6338
    Signed-off-by: Peter Grzybowski <peter@northern.tech>

--- client/common.go
+++ client/common.go
@@ -1,16 +1,16 @@
-// Copyright 2022 Northern.tech AS
+// Copyright 2023 Northern.tech AS
 //
-//    Licensed under the Apache License, Version 2.0 (the "License");
-//    you may not use this file except in compliance with the License.
-//    You may obtain a copy of the License at
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
 //
-//        http://www.apache.org/licenses/LICENSE-2.0
+//	    http://www.apache.org/licenses/LICENSE-2.0
 //
-//    Unless required by applicable law or agreed to in writing, software
-//    distributed under the License is distributed on an "AS IS" BASIS,
-//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//    See the License for the specific language governing permissions and
-//    limitations under the License.
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
 package client
 
 import (
@@ -46,6 +46,37 @@ func JoinURL(base, url string) string {
 
 func DoGetRequest(token, urlPath string, client *http.Client) ([]byte, error) {
 	req, err := http.NewRequest(http.MethodGet, urlPath, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "Failed to create HTTP request")
+	}
+	req.Header.Set("Authorization", "Bearer "+string(token))
+
+	reqDump, err := httputil.DumpRequest(req, false)
+	if err != nil {
+		return nil, err
+	}
+	log.Verbf("sending request: \n%s", string(reqDump))
+
+	rsp, err := client.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, fmt.Sprintf("Get %s request failed", urlPath))
+	}
+	if rsp.StatusCode != 200 {
+		return nil, fmt.Errorf("Get %s request failed with status %d\n", urlPath, rsp.StatusCode)
+	}
+
+	defer rsp.Body.Close()
+
+	body, err := ioutil.ReadAll(rsp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func DoPostRequest(token, urlPath string, client *http.Client) ([]byte, error) {
+	req, err := http.NewRequest(http.MethodPost, urlPath, nil)
 	if err != nil {
 		return nil, errors.Wrap(err, "Failed to create HTTP request")
 	}
